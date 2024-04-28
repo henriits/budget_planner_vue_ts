7e4717430d4f117571a1cd294bdf45d1d33b0a49
henriits <henritsarents@gmail.com>
2024-04-28 13:20:24 +0300

    ci: remove all unneeded

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,1 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-</script>
-
-<template>
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-    </nav>
-    <header></header>
-
-    <RouterView />
-</template>
+<template>hi</template>
